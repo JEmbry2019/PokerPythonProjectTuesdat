2bd5d2c320b1a91a7cb154b18bef336dcf2fa30b
JEmbry2019 <jmsembry@gmail.com>
2020-07-21 16:42:30 -0400

    Tests corrected and print statements added

--- tests.py
+++ tests.py
@@ -1,48 +1,72 @@
 import unittest
-import card
-import hand
-import game
-import deck
 
 from card import Card
 from deck import Deck
 from hand import Hand
 from game import Game
 
+""" This file contains 3 tests.
+    Tested are the add_card(), calculate_rank(), and the deal() functions.
+    Some print statements were added for clearity.
 
+"""
 
 
 class HandTestCase(unittest.TestCase):
 
-    def test_add_card_construction(self):
-        cards = Card(8, 'Clubs')
 
-        self.assertEqual(cards.rank, 'Clubs')
-        self.assertEqual(cards.suit, 8)
+    
+#  # Test 1 -A Card object is added to the cards list.
+    def test_add_card(self):
 
+        hand = Hand()
+        self.cards = []
+        initial_size = len(hand.cards)
+        print(initial_size)
+        hand.add_card(self)
+        size = len(hand.cards)
+        print(size)
 
-# class HandTestCase(unittest.TestCase):
-#     def test_three_kings_is_bust(self):
-#         hand = Hand()
-#         hand.add_card(Card(13, Card.SPADES))
-#         hand.add_card(Card(13, Card.HEARTS))
-#         hand.add_card(Card(13, Card.CLUBS))
-
-#         self.assertTrue(hand.is_bust())
-
-#  def player_is_over(self):
-#         return self.player_hand.get_rank() > 21
-
-# class GameTestCase(unittest.TestCase):
-#     def test_player_is_over_21(self):
-#         game = Game()
-
-#         game.player_hand.add_card(Hand(13, Hand.Hearts))
-#         game.player_hand.add_card(Hand(13, Hand.Clubs))
+        self.assertEqual(size, initial_size + 1)
 
 
-#         return self.player_hand.get_rank() > 21
+#----------------------------------------------        
 
+    # def test_add_card_construction(self):
+    #     cards = Card(8, 'Clubs')
+
+    #     self.assertEqual(cards.rank, 'Clubs')
+    #     self.assertEqual(cards.suit, 8)
+
+
+# ---------------------------------
+
+# Test 2 - This function calculates rank correctly.   
+    def test_calculate_rank(self):
+
+        hand = Hand()
+        self.rank = 'Q'
+        hand.calculate_rank()
+       
+        self.assertTrue(self.rank, 12)
+
+
+
+
+#-------------------------------------
+
+# Test 3 This function makes sure the deck has more than 1 card.
+class DeckTestCase(unittest.TestCase):
+
+    def test_deal_checks_deck_greater_than_1(self):
+        deck = Deck()
+        initial_size = len(deck.cards)
+        print(initial_size)
+        deck.deal()
+
+        self.assertGreater(len(deck.cards), 2)
+
+        
 
 
 if __name__ == '__main__':
